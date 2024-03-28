cf8da02aa55599fa5d61ab65d8c49a3a4acb72c7
NikitaEfrem <ahmadibragprogib@gmail.com>
2024-03-28 15:11:35 +0300

    Task 4.6
    Task 4.6

---
+++ Tasks/Tasks 4/Task 4.6/Program.cs
@@ -0,0 +1,10 @@
+﻿int birthday = 19;
+
+var numbers = Enumerable.Range(100, 700);
+
+var div = numbers.Where(num => num % birthday == 0);
+
+foreach (var num in div)
+{
+    Console.WriteLine(num);
+}
