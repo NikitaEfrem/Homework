5e366491ee6bc38e6628ff01344f6d73044bd805
NikitaEfrem <ahmadibragprogib@gmail.com>
2024-03-28 14:53:07 +0300

    Task 4.4
    Task 4.4

---
+++ Tasks/Tasks 4/Task 4.4/Program.cs
@@ -0,0 +1,22 @@
+﻿ConsoleKeyInfo info = Console.ReadKey();
+
+if (info.Key == ConsoleKey.W)
+{
+    Console.WriteLine(" The figure needs to be moved up");
+}
+else if (info.Key == ConsoleKey.S)
+{
+    Console.WriteLine(" The figure needs to be moved back");
+}
+else if(info.Key == ConsoleKey.A)
+{
+    Console.WriteLine(" The figure needs to be moved left");
+}
+else if (info.Key == ConsoleKey.D)
+{
+    Console.WriteLine(" The figure needs to be moved right");
+}
+else
+{
+    Console.WriteLine(" No movement is needed");
+}
