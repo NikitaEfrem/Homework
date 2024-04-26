94d891b6f041c85c2f7f12a8c383edcdcde874b9
NikitaEfrem <ahmadibragprogib@gmail.com>
2024-04-26 21:33:43 +0300

    d

--- Tasks/Homework/2midtermtest/Program.cs
+++ Tasks/Homework/2midtermtest/Program.cs
@@ -3,22 +3,7 @@ using System.Text;
 
 namespace _2midtermtest
 {
-    public static class StringBuilderExtensions
-    {
-        public static StringBuilder ToUpperString(this StringBuilder stringBuilder)
-        {
-            string vowels = "aeiouAEIOU";
-            for (int i = 0; i < stringBuilder.Length; i++)
-            {
-                if (vowels.Contains(stringBuilder[i].ToString()))
-                {
-                    stringBuilder[i] = char.ToUpper(stringBuilder[i]);
-                }
-            }
-            return stringBuilder;
-        }
-    }
-
+   
     class Program
     {
         static void Main()
---
+++ Tasks/Homework/2midtermtest/StringBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+
+
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder ToUpperString(this StringBuilder stringBuilder)
+        {
+            string vowels = "aeiouAEIOU";
+            for (int i = 0; i < stringBuilder.Length; i++)
+            {
+                if (vowels.Contains(stringBuilder[i].ToString()))
+                {
+                    stringBuilder[i] = char.ToUpper(stringBuilder[i]);
+                }
+            }
+            return stringBuilder;
+        }
+    }
